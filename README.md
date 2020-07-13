f96593924241ab027c73b148e097cec29cc18229
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-07-13 13:40:24 +0200

    reordered and minified files now shall render from docs
    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# The objective
+
+  - primary: present my projects
+  - secondary: present my skills
+
+
+## Technologies
+
+ - SASS/SCSS
+ - JS
+ 
+
+## Installation
+- 
+- That site is hosted on GH Pages
+
+## Browser Support
+
+- Not for IE <10 and Opera Mini
+
+## Versions
+##### 1 
+- initial
+
+
+### To do
+
+more webp
+optimise images
---
+++ docs/js/init.js
@@ -0,0 +1,60 @@
+function throttle(func, ms) {
+  
+    let isThrottled = false, savedArgs, savedThis;
+  
+      function wrapper() {
+  
+        if (isThrottled) { 
+          savedArgs = arguments;
+          savedThis = this;
+          return;
+        }
+  
+        func.apply(this, arguments); 
+        isThrottled = true;
+  
+        setTimeout(function() {
+          isThrottled = false; 
+          if (savedArgs) {
+            wrapper.apply(savedThis, savedArgs);
+            savedArgs = savedThis = null;
+          }
+        }, ms);
+      }
+  
+  return wrapper;
+  }
+
+/////////// assures proper repositioning of intro  
+window.onload = function () {
+
+  var element = document.getElementById('intro');
+  const loader = document.getElementById('loader-wrapper');
+  element.classList.add('regular');
+  document.getElementById('informations').style.marginTop = document.getElementById('heading').clientHeight + "px";
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+
+  setTimeout(function () {
+    new ResizeSensor(element, function () {
+      document.getElementById('informations').style.marginTop = element.clientHeight + "px";
+    });
+  }, 5000)
+
+  function change_location(ev) {
+    location.hash = ev.target.dataset.target;
+    window.scrollBy(0, -document.getElementById('intro').clientHeight);
+  }
+
+  change_location = throttle(change_location, 500);
+
+  Array.prototype.forEach.call(
+    document.getElementsByClassName('location-button'),
+    button => {
+      button.addEventListener('click', change_location);
+    }
+  );
+
+}
