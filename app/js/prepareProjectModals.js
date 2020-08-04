d092dfd1c790a74ee14e9eea5f557b4b61768f5c
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-08-04 14:32:41 +0200

    clearer files location
    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

---
+++ app/js/prepareProjectModals.js
@@ -0,0 +1,37 @@
+module.exports = {
+  prepareProjectModals: function prepareProjectModals(modal) {
+    let smallProjectCovers = document.getElementsByClassName("project-pointer");
+
+    function showModal(ev) {
+      
+      modal.innerHTML = ev.target.parentNode.innerHTML;
+      
+      modal.removeChild(modal.getElementsByClassName("project__name pointer")[0]);
+      modal.removeChild(modal.getElementsByClassName("left-cover")[0]);
+      modal.removeChild(modal.getElementsByClassName('right-cover')[0]);
+      modal.getElementsByClassName("initial")[0].classList.toggle("initial");
+
+      const delete_button = document.createElement("div");
+      delete_button.classList.add("icon-delete");
+      delete_button.tabIndex = 0;
+      delete_button.addEventListener("click", hide_modal);
+
+      function hide_modal() {
+        delete_button.removeEventListener("click", hide_modal);
+        modal.innerHTML = "";
+      }
+      modal.appendChild(delete_button);
+      modal.classList.add("large");
+      modal.getElementsByClassName("project__image")[0].classList.add("large");
+      modal.style.display = "block";
+
+      location.hash ='large-project-content';
+      window.scrollBy(0, -document.getElementById("intro").clientHeight);
+    }
+    showModal = typeof throttle !== "undefined" ? throttle(showModal, 500) : showModal;
+    Array.prototype.forEach.call(smallProjectCovers, (cover) => {
+      cover.addEventListener("click", showModal);
+      cover.addEventListener("keyup", function(event){if(event.keyCode===13){showModal(event)};});
+    });
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -25,7 +25,7 @@
 
 
 ### To do
-powinno mieć obsługę zdzrzenia kliknięcia enter na wsyztsko skoro jest klikalne klawiaturą
+
 może bardziej wyraziste nazwy portfoliów w sensie większy tekst itp
 fallback dla lazy loading native
 render ścieżi krytycznej poprawić
@@ -49,18 +49,3 @@ if ('loading' in HTMLImageElement.prototype) {
 
 JS
 
-
-
-In here, someFunction uses a module imported from a particular library. If this module is not being used elsewhere, the code block can be modified to use a dynamic import to fetch it only when the form is submitted by the user.
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  import('library.moduleA')
-    .then(module => module.default) // using the default export
-    .then(someFunction())
-    .catch(handleError());
-});
-
-const someFunction = () => {
-    // uses moduleA
-}
