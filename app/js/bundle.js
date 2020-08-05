41df9088ea2ed08f666ddebc280336dcc1c4f4f1
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-08-05 09:52:11 +0200

    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl> preload background upto media query
    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -149,7 +149,7 @@ module.exports = {
   prepareFooter: function prepareFooter(target) {
     const footer = document.createElement("footer");
     footer.classList.add('ui-footer');
-    footer.innerHTML = "<svg viewBox='0 0 500 150' preserveAspectRatio='none' style='height: 100%; width: 100%; position:absolute; bottom:0; left:0;'><path d= 'M0.27,1.47 C222.62,412.00 311.23,-263.98 499.72,150.48 L500.27,153.45 L0.00,150.00 Z' style='stroke: none;fill:#8083FF;'></path></svg><img class='gif gif-footer' src='../graphics/gifs/dancer.webp' alt='dancer' /><strong><p>Created by Piotr Maksymiuk | 2020. © Wszelkie prawa zastrzeżone</p></strong>"
+    footer.innerHTML = "<svg viewBox='0 0 500 150' preserveAspectRatio='none' style='height: 100%; width: 100%; position:absolute; bottom:0; left:0;'><path d= 'M0.27,1.47 C222.62,412.00 311.23,-263.98 499.72,150.48 L500.27,153.45 L0.00,150.00 Z' style='stroke: none;fill:#8083FF;'></path></svg><img class='gif gif-footer' src='./graphics/gifs/dancer.webp' alt='dancer' /><strong><p>Created by Piotr Maksymiuk | 2020. © Wszelkie prawa zastrzeżone</p></strong>"
     target.appendChild(footer);
   },
 };
@@ -171,10 +171,11 @@ module.exports = {
         toggleMenuVisibility();
       }
     });
-  }else{console.log('can not open hamburger mnu - missing or falsey arguments')}
+  }else{console.log('can not open hamburger menu - missing or falsey arguments')}
   },
 };
 
+//s
 },{}],6:[function(require,module,exports){
 module.exports = {
   prepareProjectModals: function prepareProjectModals(modal) {
@@ -193,11 +194,6 @@ module.exports = {
       delete_button.classList.add("icon-delete");
       delete_button.tabIndex = 0;
       delete_button.addEventListener("click", hide_modal);
-
-    
-
-
-
 
       function hide_modal() {
         delete_button.removeEventListener("click", hide_modal);
