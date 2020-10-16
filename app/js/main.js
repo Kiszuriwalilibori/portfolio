b707dcf06af5309984ef2af1582952d85e188999
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-10-16 10:28:16 +0200

    better ARIA support
    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

--- app/js/main.js
+++ app/js/main.js
@@ -34,10 +34,6 @@ window.onload = function () {
   const body = document.getElementsByTagName('body')[0];
   const iconDeleteEmailModal = document.getElementById("emailModal-deleteIcon");
 
-  // if (!(informations && heading && intro && locationButtons && mailButtons && emailModal && hamburger && menu &&introUIAside && body && iconDeleteEmailModal)) {
-  //   window.alert("Nie odnaleziono jednego lub więcej ważnych identyfikatorów. Strona nie będzie działać proawidłowo");
-  //   return false;
-  // }
   
   prepareResizeSensor(informations, intro, heading);
   prepareChangeLocation(locationButtons);
